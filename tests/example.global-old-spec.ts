997b52e3968d8d0022f6df95550513a6fe64f438
Kanthavel Pasupathipillai <kanthavel.pasupathipillai@spindox.it>
2025-04-11 09:46:39 +0200

    WIP reworking tests

--- tests/example.global-old-spec.ts
+++ tests/example.global-old-spec.ts
@@ -3,16 +3,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from 'apps/api/src/app.module';
 import { CliProgram } from '@sermas/cli/dist/cli';
-import { run } from '@sermas/cli/dist/commands/app/batch';
-import { CommandParams } from '@sermas/cli/dist/libs/dto/cli.dto';
+import { CliProgram } from '@sermas/cli/dist/cli/';
 
 describe('TTS (e2e)', () => {
   let app: INestApplication;
   let cli: CliProgram;
-  const testPort: number = 3000; // Matches port expected by Caddyfile
+  const testPort: number = 1979;
 
   beforeAll(async () => {
     jest.setTimeout(15 * 1000);
+    process.env.BASE_URL = `http://localhost:${testPort}`;
+    process.env.REDIS_URL = 'redis://localhost:6379';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/sermas';
+    process.env.MQTT_URL = 'localhost';
+    process.env.CHROMA_URL = 'http://localhost:8007';
+    process.env.MINIO_URL = 'http://localhost:9000';
   });
 
   beforeEach(async () => {
@@ -57,17 +62,19 @@ describe('TTS (e2e)', () => {
   it(
     'should run poa tests',
     async () => {
-      const commandParams: CommandParams = {
-        flags: undefined,
-        args: [],
-        feature: undefined,
-        command: new Command,
-        program: new Command,
-        api: new CliApi
-      };
-      const stats = run(commandParams);
       // Just a Dummy
-      expect(stats).toEqual(42);
+      expect(
+        await cli.parse(
+          [
+            '--log-level',
+            'verbose',
+            'app',
+            'batch',
+            '../../tmp/repository/labs/sermas-private/apps/poa',
+          ],
+          { from: 'user' },
+        ),
+      ).toEqual(42);
     },
     15 * 1000,
   );
