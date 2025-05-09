740e51b879f6a23618f09c528e40e7ada473a010
lc-spxl <luca.capra@spindox.it>
2025-05-09 18:16:21 +0200

    add clear screen on task started or aborted

--- apps/ui/src/ui.service.events.ts
+++ apps/ui/src/ui.service.events.ts
@@ -1,9 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { DialogueTaskProgressDto } from 'apps/dialogue/src/tasks/dialogue.tasks.dto';
 import { SessionChangedDto } from 'apps/session/src/session.dto';
 import { Payload, Subscribe } from 'libs/mqtt-handler/mqtt.decorator';
 import { SermasTopics } from 'libs/sermas/sermas.topic';
-import { UIContentDto } from './ui.content.dto';
+import { ClearScreenDto, UIContentDto } from './ui.content.dto';
 import { UIService } from './ui.service';
 
 @Injectable()
@@ -25,5 +26,15 @@ export class UIEventsService {
   @OnEvent('session.changed')
   async onSessionChanged(content: SessionChangedDto) {
     this.ui.onSessionChanged(content);
+  }
+
+  @OnEvent('task.progress')
+  // if a task is removed, abort the ongoing records
+  async trackProgress(ev: DialogueTaskProgressDto) {
+    if (!ev.record?.sessionId) return;
+    if (ev.type !== 'started' && ev.type !== 'aborted') return;
+
+    this.logger.debug(`Task ${ev.task.name} ${ev.type}, clearing screen`);
+    await this.ui.sendClearScreen(ev.record);
   }
 }
